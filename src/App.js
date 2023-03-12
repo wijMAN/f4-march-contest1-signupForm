2746e8992a946fad592922f3835fb9311d3144fe
wijman <wish6242@gmail.com>
2023-03-12 22:33:15 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello world! is my thing :)</p>
         <button>Click me :)</button>
+        <br />
+        <br />
         <a
           className="App-link"
           href="https://reactjs.org"
