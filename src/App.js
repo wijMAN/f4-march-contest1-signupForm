399db60eaece82a68dd98c9cb233ba45e751d3ff
wijman <wish6242@gmail.com>
2023-03-12 17:34:41 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello world! is my thing :)</p>
-        <button>Click me</button>
+        <button>Click me :)</button>
         <a
           className="App-link"
           href="https://reactjs.org"
